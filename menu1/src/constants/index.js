e78f913c840ddca3559468c419e94bdfd144a0e3
Nirmalya Pal <79367312+SpEXterXD@users.noreply.github.com>
2023-10-28 16:34:50 +0530

    Menu Template #1

---
+++ menu1/src/constants/index.js
@@ -0,0 +1,18 @@
+export const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Work",
+    href: "/work",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+]
